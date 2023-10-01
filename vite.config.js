401a4c7d706a939ad98e4025507a6846d2adff47
Fredrik Andersson <fredrik.ks.andersson@gmail.com>
2023-10-01 18:19:44 +0200

    use root index.html

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
-import fs from 'fs/promises';
 
 export default defineConfig({
   server: {
     port: 4200
-  },
-  plugins: [{
-    name: 'index-html-build-replacement',
-    async transformIndexHtml() {
-      return await fs.readFile('./src/html/index.html', 'utf8');
-    }
-  }],
+  }
 })
